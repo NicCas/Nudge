8c93d21071e9263b82ba3e3482cb1a5bdf1cb46a
Richard Casillas <47794091+RichardCasillas@users.noreply.github.com>
2021-05-08 18:39:57 -0700

    Update MainActivity.kt
    new changes for navigation bar

--- app/src/main/java/com/bignerdranch/android/nudge/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/nudge/MainActivity.kt
@@ -2,10 +2,38 @@ package com.bignerdranch.android.nudge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.bignerdranch.android.nudge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding   //view binding variable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)  //prevents app from going into night mode
+
+        setUpTabBar()
+    }
+
+    //method to set up the nav bar and what is displayed
+    private fun setUpTabBar() {
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when (it) {
+                R.id.nav_view -> binding.textMain.text = "Calendar View"
+                R.id.nav_edit -> binding.textMain.text = "Calendar Edit"
+                R.id.nav_ht -> {
+                    binding.textMain.text = "Habit Tracker"
+                    binding.bottomNavBar.showBadge(R.id.nav_settings)
+                }
+                R.id.nav_settings -> {
+                    binding.textMain.text = "Settings"
+                    binding.bottomNavBar.dismissBadge(R.id.nav_settings)
+                }
+            }
+        }
     }
 }
