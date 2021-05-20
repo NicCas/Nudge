0d4d57c114932958f20a6cdb5859685cbf94e9e7
Nicole C <nicas@sonic.net>
2021-05-19 23:04:09 -0700

    Push non functioning calendar

--- app/src/main/java/com/bignerdranch/android/nudge/fragments/CalendarViewFragment.kt
+++ app/src/main/java/com/bignerdranch/android/nudge/fragments/CalendarViewFragment.kt
@@ -7,6 +7,284 @@ import android.view.View
 import android.view.ViewGroup
 import com.bignerdranch.android.nudge.R
 
+// Code is sampled from set up instructions provoded but the Calendar library creator
+import android.os.Bundle
+import android.provider.Settings.Global.getString
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.children
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.internal.ViewUtils.dpToPx
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import com.kizitonwose.calendarviewsample.databinding.Example3CalendarDayBinding
+import com.kizitonwose.calendarviewsample.databinding.Example3CalendarHeaderBinding
+import com.kizitonwose.calendarviewsample.databinding.Example3EventItemViewBinding
+import com.kizitonwose.calendarviewsample.databinding.Example3FragmentBinding
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+data class Event(val id: String, val text: String, val date: LocalDate)
+
+class Example3EventsAdapter(val onClick: (Event) -> Unit) :
+        RecyclerView.Adapter<Example3EventsAdapter.Example3EventsViewHolder>() {
+
+    val events = mutableListOf<Event>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Example3EventsViewHolder {
+        return Example3EventsViewHolder(
+                EventItemViewBinding.inflate(parent.context.layoutInflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(viewHolder: Example3EventsViewHolder, position: Int) {
+        viewHolder.bind(events[position])
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    inner class Example3EventsViewHolder(private val binding: EventItemViewBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener {
+                onClick(events[bindingAdapterPosition])
+            }
+        }
+
+        fun bind(event: Event) {
+            binding.itemEventText.text = event.text
+        }
+    }
+}
+
+class Example3Fragment : BaseFragment(R.layout.calendar_fragment), HasBackButton {
+
+    private val eventsAdapter = Example3EventsAdapter {
+        AlertDialog.Builder(requireContext())
+                .setMessage(R.string.dialog_delete_confirmation)
+                .setPositiveButton(R.string.delete) { _, _ ->
+                    deleteEvent(it)
+                }
+                .setNegativeButton(R.string.close, null)
+                .show()
+    }
+
+    private val inputDialog by lazy {
+        val editText = AppCompatEditText(requireContext())
+        val layout = FrameLayout(requireContext()).apply {
+            // Setting the padding on the EditText only pads the input area
+            // not the entire EditText so we wrap it in a FrameLayout.
+            val padding = dpToPx(20, requireContext())
+            setPadding(padding, padding, padding, padding)
+            addView(editText, FrameLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT))
+        }
+        AlertDialog.Builder(requireContext())
+                .setTitle(getString(R.string.input_dialog_title))
+                .setView(layout)
+                .setPositiveButton(R.string.save) { _, _ ->
+                    saveEvent(editText.text.toString())
+                    // Prepare EditText for reuse.
+                    editText.setText("")
+                }
+                .setNegativeButton(R.string.close, null)
+                .create()
+                .apply {
+                    setOnShowListener {
+                        // Show the keyboard
+                        editText.requestFocus()
+                        context.inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+                    }
+                    setOnDismissListener {
+                        // Hide the keyboard
+                        context.inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+                    }
+                }
+    }
+
+    override val titleRes: Int = R.string.example_3_title
+
+    private var selectedDate: LocalDate? = null
+    private val today = LocalDate.now()
+
+    private val titleSameYearFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val titleFormatter = DateTimeFormatter.ofPattern("MMM yyyy")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
+    private val events = mutableMapOf<LocalDate, List<Event>>()
+
+    private lateinit var binding: CalendarFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = CalendarFragmentBinding.bind(view)
+        binding.exThreeRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            adapter = eventsAdapter
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+        }
+
+        val daysOfWeek = daysOfWeekFromLocale()
+        val currentMonth = YearMonth.now()
+        binding.exThreeCalendar.apply {
+            setup(currentMonth.minusMonths(10), currentMonth.plusMonths(10), daysOfWeek.first())
+            scrollToMonth(currentMonth)
+        }
+
+        if (savedInstanceState == null) {
+            binding.exThreeCalendar.post {
+                // Show today's events initially.
+                selectDate(today)
+            }
+        }
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay // Will be set when this container is bound.
+            val binding = CalendarDayBinding.bind(view)
+
+            init {
+                view.setOnClickListener {
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        selectDate(day.date)
+                    }
+                }
+            }
+        }
+        binding.exThreeCalendar.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                val textView = container.binding.exThreeDayText
+                val dotView = container.binding.exThreeDotView
+
+                textView.text = day.date.dayOfMonth.toString()
+
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    textView.makeVisible()
+                    when (day.date) {
+                        today -> {
+                            textView.setTextColorRes(R.color.white)
+                            textView.setBackgroundResource(R.drawable.today_bg)
+                            dotView.makeInVisible()
+                        }
+                        selectedDate -> {
+                            textView.setTextColorRes(R.color.blue)
+                            textView.setBackgroundResource(R.drawable.selected_bg)
+                            dotView.makeInVisible()
+                        }
+                        else -> {
+                            textView.setTextColorRes(R.color.black)
+                            textView.background = null
+                            dotView.isVisible = events[day.date].orEmpty().isNotEmpty()
+                        }
+                    }
+                } else {
+                    textView.makeInVisible()
+                    dotView.makeInVisible()
+                }
+            }
+        }
+
+        binding.exThreeCalendar.monthScrollListener = {
+            homeActivityToolbar.title = if (it.year == today.year) {
+                titleSameYearFormatter.format(it.yearMonth)
+            } else {
+                titleFormatter.format(it.yearMonth)
+            }
+
+            // Select the first day of the month when
+            // we scroll to a new month.
+            selectDate(it.yearMonth.atDay(1))
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val legendLayout = CalendarHeaderBinding.bind(view).legendLayout.root
+        }
+        binding.exThreeCalendar.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                // Setup each header day text if we have not done that already.
+                if (container.legendLayout.tag == null) {
+                    container.legendLayout.tag = month.yearMonth
+                    container.legendLayout.children.map { it as TextView }.forEachIndexed { index, tv ->
+                        tv.text = daysOfWeek[index].name.first().toString()
+                        tv.setTextColorRes(R.color.black)
+                    }
+                }
+            }
+        }
+
+        binding.exThreeAddButton.setOnClickListener {
+            inputDialog.show()
+        }
+    }
+
+    private fun selectDate(date: LocalDate) {
+        if (selectedDate != date) {
+            val oldDate = selectedDate
+            selectedDate = date
+            oldDate?.let { binding.exThreeCalendar.notifyDateChanged(it) }
+            binding.exThreeCalendar.notifyDateChanged(date)
+            updateAdapterForDate(date)
+        }
+    }
+
+    private fun saveEvent(text: String) {
+        if (text.isBlank()) {
+            Toast.makeText(requireContext(), R.string.empty_input_text, Toast.LENGTH_LONG).show()
+        } else {
+            selectedDate?.let {
+                events[it] = events[it].orEmpty().plus(Event(UUID.randomUUID().toString(), text, it))
+                updateAdapterForDate(it)
+            }
+        }
+    }
+
+    private fun deleteEvent(event: Event) {
+        val date = event.date
+        events[date] = events[date].orEmpty().minus(event)
+        updateAdapterForDate(date)
+    }
+
+    private fun updateAdapterForDate(date: LocalDate) {
+        eventsAdapter.apply {
+            events.clear()
+            events.addAll(this@CalenderActivity.events[date].orEmpty())
+            notifyDataSetChanged()
+        }
+        binding.exThreeSelectedDateText.text = selectionFormatter.format(date)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        homeActivityToolbar.setBackgroundColor(requireContext().getColorCompat(R.color.toolbar_color))
+        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.statusbar_color)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        homeActivityToolbar.setBackgroundColor(requireContext().getColorCompat(R.color.colorPrimary))
+        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.colorPrimaryDark)
+    }
+}
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
